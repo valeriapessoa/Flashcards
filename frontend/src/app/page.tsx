92d57727d478083b11d57bb525db7748ee11c17f
Valéria Pessoa <valeriapessoa2005@gmail.com>
2025-06-14 01:30:58 -0300

    feat: remove imports não utilizados e limpa o código da página inicial

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 
-import { Card, CardContent, Grid, Box, Tooltip, Divider, Avatar, Fade, Rating, Typography } from "@mui/material";
-import { useSession, signOut } from "next-auth/react";
-import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import Image from "next/image";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import TestimonialsSection from "../components/TestimonialsSection";
@@ -12,11 +9,7 @@ import HeroSection from "../components/HeroSection";
 import HowItWorksSection from "../components/HowItWorksSection";
 import BenefitsSection from "../components/BenefitsSection";
 import FeatureHighlightSection from "../components/FeatureHighlightSection";
-import AccessTimeIcon from '@mui/icons-material/AccessTime';
-import StarIcon from '@mui/icons-material/Star';
-import BookmarkIcon from '@mui/icons-material/Bookmark';
-import StarHalfIcon from '@mui/icons-material/StarHalf';
-import StarBorderIcon from '@mui/icons-material/StarBorder';
+
 export default function Home() {
   const { data: session } = useSession();
   const router = useRouter();
@@ -28,7 +21,6 @@ export default function Home() {
       <HowItWorksSection />
       <BenefitsSection />
       <FeatureHighlightSection />
-
       <TestimonialsSection />
       <Footer />
     </>
